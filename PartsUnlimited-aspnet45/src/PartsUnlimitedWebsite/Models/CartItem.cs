1621ec5db126e76db5800c63cb20fa46718156f4
Sergio Moreno <s.moreno.sanmiguel@accenture.com>
2020-12-16 11:56:48 -0800

    Segundo commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // Primer cambio
+        // Segundo cambio
         [Key]
         public int CartItemId { get; set; }
 
